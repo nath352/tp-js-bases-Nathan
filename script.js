e6363af57e2120bfeb14d85ab7064000164e1633
nath352 <nathanmichon09@gmail.com>
2025-11-06 14:28:32 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Laboratoire prêt !")
+const nom = "Nathan"; 
+let age = 18;
+/nom = "Un autre nom";/
+console.log(age)
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "12345";      
+if (motDePasseUtilisateur===motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (motDePasseUtilisateur.length<8) {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+ } else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
